895f3c8e9fe0b81343225c415d897fc93ddfe066
Jan Vermeir <jvermeir@xebia.com>
2014-12-20 22:14:54 +0100

    removed unnecessary check on contents list.

--- src/main/java/nl/vermeir/epub/EPubMerger.java
+++ src/main/java/nl/vermeir/epub/EPubMerger.java
@@ -39,11 +39,11 @@ public class EPubMerger {
 
     public void mergeLines(Book book) throws Exception {
         List<Resource> contents = book.getContents();
-        int firstContentItem = 2;
-        if (contents.size() < 3) {
-            firstContentItem = 0;
-        }
-        for (int contentItem = firstContentItem; contentItem < contents.size(); contentItem++) {
+//        int firstContentItem = 2;
+//        if (contents.size() < 3) {
+//            firstContentItem = 0;
+//        }
+        for (int contentItem = 0; contentItem < contents.size(); contentItem++) {
             Resource resource = contents.get(contentItem);
             String text = new String(resource.getData());
             String[] lines = text.split("\n");
